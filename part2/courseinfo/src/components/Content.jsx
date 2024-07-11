d53e0e5fdf1669b0c00a21947ae32b8fce6976f9
Vibhor Gautam <vibgofficial@gmail.com>
2024-07-11 18:26:15 -0400

    exercise 2.2 - 2.5 fso

--- part2/courseinfo/src/components/Content.jsx
+++ part2/courseinfo/src/components/Content.jsx
@@ -1,11 +1,14 @@
 import Part from "./Part";
+import Total from './Total';
 
 const Content = ({ parts }) => {
+    const total = parts.reduce((sum, part) => sum + part.exercises, 0);
     return (
         <div>
         {parts.map( part => {
             return (<Part key={part.id} part={part} />)
         })}
+        <Total total={total} />
         </div>
     )
 }
---
+++ part2/courseinfo/src/components/Total.jsx
@@ -0,0 +1,9 @@
+const Total = ({ total }) => {
+    return (
+        <div>
+            <b>Total number of exercises: {total}</b>
+        </div>
+    )
+}
+
+export default Total;
