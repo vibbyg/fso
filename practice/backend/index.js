d1bb37db169fdbe33445599ae1c882bbc877bdb0
Vibhor Gautam <vibgofficial@gmail.com>
2024-07-23 14:15:48 -0400

    updating practice files

--- practice/backend/index.js
+++ practice/backend/index.js
@@ -1,7 +1,16 @@
 const express = require('express')
 const app = express()
 
+const requestLogger = (request, response, next) => {
+    console.log('Method:', request.method)
+    console.log('Path:', request.path)
+    console.log('Body:', request.body)
+    console.log('---')
+    next()
+}
+
 app.use(express.json())
+app.use(requestLogger)
 
 let notes = [
     {
@@ -68,6 +77,11 @@ app.post('/api/notes', (request, response) => {
     notes = notes.concat(note);
     response.json(note)
 })
+
+const unknownEndpoint = (request, response) => {
+    return response.status(404).send({ error: 'unknown endpoint' })
+}
+app.use(unknownEndpoint)
 
 const PORT = 3001
 app.listen(PORT)
