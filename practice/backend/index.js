03b30eccfaaa57c70bf7a26895cb0f0434808ea9
Vibhor Gautam <vibgofficial@gmail.com>
2024-07-25 11:50:59 -0400

    practice 3c, implementing delete & put & error handling fso

--- practice/backend/index.js
+++ practice/backend/index.js
@@ -12,28 +12,24 @@ const requestLogger = (request, response, next) => {
     next()
 }
 
+const unknownEndpoint = (request, response) => {
+    return response.status(404).send({ error: 'unknown endpoint' })
+}
+
+const errorHandler = (error, request, response, next) => {
+    console.error(error.message)
+
+    if (error.name === 'CastError') {
+        return response.status(400).send({ error: 'malformatted id' })
+    }
+
+    next(error)
+}
+
 app.use(express.json())
 app.use(cors())
 app.use(requestLogger)
 app.use(express.static('dist'))
-
-let notes = [
-    {
-      id: "1",
-      content: "HTML is easy",
-      important: true
-    },
-    {
-      id: "2",
-      content: "Browser can execute only JavaScript",
-      important: false
-    },
-    {
-      id: "3",
-      content: "GET and POST are the most important methods of HTTP protocol",
-      important: true
-    }
-  ]
   
 app.get('/', (request, response) => {
     response.send('<h1>Hello World</h1>')
@@ -45,16 +41,29 @@ app.get('/api/notes', (request, response) => {
     })
 })
 
-app.get('/api/notes/:id', (request, response) => {
+app.get('/api/notes/:id', (request, response, next) => {
     Note.findById(request.params.id).then(note => {
-        response.json(note)
+        if (note) {
+            response.json(note)
+
+        } else {
+            response.status(404).end()
+        }
+    })
+    .catch(error => {
+        next(error)
     })
 })
 
-app.delete('/api/notes/:id', (request, response) => {
-    const id = request.params.id
-    notes = notes.filter(note => note.id !== id)
-    response.status(204).end()
+app.delete('/api/notes/:id', (request, response, next) => {
+
+    Note.findByIdAndDelete(request.params.id)
+    .then(deletedNote => {
+        response.json(deletedNote).end()
+    })
+    .catch(error => {
+        next(error)
+    })
 })
 
 app.post('/api/notes', (request, response) => {
@@ -73,10 +82,24 @@ app.post('/api/notes', (request, response) => {
     })
 })
 
-const unknownEndpoint = (request, response) => {
-    return response.status(404).send({ error: 'unknown endpoint' })
-}
+app.put('/api/notes/:id', (request, response, next) => {
+    const body = request.body
+    const note = {
+        content: body.content,
+        important: body.important
+    }
+
+    Note.findByIdAndUpdate(request.params.id, note, { new: true} )
+    .then(updatedNote => {
+        response.json(updatedNote)
+    })
+    .catch(error => {
+        next(error)
+    })
+})
+
 app.use(unknownEndpoint)
+app.use(errorHandler)
 
 const PORT = process.env.PORT
 app.listen(PORT)
